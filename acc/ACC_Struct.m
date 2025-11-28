ebf7350eb893bb9fc6c8ffa95082c2ce71a54faa
Matthew Keller <kellermatthewe@gmail.com>
2025-11-27 21:32:56 -0500

    acc fixed

--- acc/ACC_Struct.m
+++ acc/ACC_Struct.m
@@ -11,7 +11,7 @@ acc_elems(3).Name = 'Longitudinal_Switch_ON';
 acc_elems(3).DataType = 'boolean';
 
 acc_elems(4) = Simulink.BusElement;
-acc_elems(4).Name = 'SET_Pressed';
+acc_elems(4).Name = 'Set_Resume';
 acc_elems(4).DataType = 'boolean';
 
 acc_elems(5) = Simulink.BusElement;
@@ -25,6 +25,10 @@ acc_elems(6).DataType = 'boolean';
 acc_elems(7) = Simulink.BusElement;
 acc_elems(7).Name = 'Timeout_Event';
 acc_elems(7).DataType = 'boolean';
+
+acc_elems(8) = Simulink.BusElement;
+acc_elems(8).Name = 'In_CACC_Speed_Range';
+acc_elems(8).DataType = 'boolean';
 
 ACCStatusBus = Simulink.Bus;
 ACCStatusBus.Elements = acc_elems;
