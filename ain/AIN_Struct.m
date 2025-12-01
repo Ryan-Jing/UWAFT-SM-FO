2edccc3a0d7b87b91cf6e9b8c809bc480c0b6a65
Matthew Keller <kellermatthewe@gmail.com>
2025-11-30 23:33:22 -0500

    manual tests added

--- ain/AIN_Struct.m
+++ ain/AIN_Struct.m
@@ -7,7 +7,7 @@ ain_elems(2).Name = 'Cancel_AIN_Pressed';
 ain_elems(2).DataType = 'boolean';
 
 ain_elems(3) = Simulink.BusElement;
-ain_elems(3).Name = 'V2X_Switch_On';
+ain_elems(3).Name = 'V2X_Switch_ON';
 ain_elems(3).DataType = 'boolean';
 
 ain_elems(4) = Simulink.BusElement;
